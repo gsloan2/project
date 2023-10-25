e05d977b4ce11e70817c7348d29f63fbc6b06d8a
gsloan20 <sloan.garrett4@yahoo.com>
2023-10-25 13:46:32 -0700

    Adding in player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 5f;
+    public Rigidbody2D playerBody;
+    private Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    Vector2 dir;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+            animator.SetFloat("X", -1);
+            animator.SetFloat("Y", 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+            animator.SetFloat("X", 1);
+            animator.SetFloat("Y", 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = 1;
+            animator.SetFloat("X", 0);
+            animator.SetFloat("Y", 1);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir.y = -1;
+            animator.SetFloat("X", 0);
+            animator.SetFloat("Y", -1);
+        }
+
+        dir.Normalize();
+        animator.SetBool("IsMoving", dir.magnitude > 0);
+
+        GetComponent<Rigidbody2D>().velocity = speed * dir;
+        
+    }
+
+
+}
